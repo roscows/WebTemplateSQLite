54eb0a28f873a04f095b6aa59ba9d2514f1e4265
Roscows <marko.milisavljevic@elfak.rs>
2025-10-18 16:42:48 +0200

    Update 1.0

--- Models/IspitContext.cs
+++ Models/IspitContext.cs
@@ -2,10 +2,20 @@ namespace WebTemplate.Models;
 
 public class IspitContext : DbContext
 {
-    // DbSet kolekcije!
+    // Dodaj svoje DbSet kolekcije ovde
+    public DbSet<Korisnik> Korisnici { get; set; }
+    // public DbSet<Proizvod> Proizvodi { get; set; }
+    // ... ostali entiteti
 
     public IspitContext(DbContextOptions options) : base(options)
     {
         
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // Ovde možeš dodati konfiguracije za entitete
     }
 }
--- obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00969aab16e8c76b728e53c9e332293758310c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
